d178c894d9df11474f57397eb6b4f172f0283c0e
Roshan <roshankayastha8@gmail.com>
2024-12-25 22:40:44 +0545

    Added the Update or Edit Note function.

--- index.php
+++ index.php
@@ -9,12 +9,26 @@ $database = "dnexa";
 
 // Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
 // Die if connection was not successful
 if(!$conn){
   die("Sorry we failed to connect: " . mysqli_connect_error());
 }
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+  if(isset($_POST['snoEdit'])){
+  // Update the recored
+  $sno = $_POST['snoEdit'];
+  $title = $_POST['titleEdit'];
+  $description = $_POST['descriptionEdit'];
+  $sql = "UPDATE `tnexa` SET `title` = '$title' , `description` = '$description' WHERE `tnexa`.`sno` = $sno;";
+  $result = mysqli_query($conn, $sql);
+  if($result){
+    echo "We updated the record";
+  }
+  else{
+    echo "We could not update the record.";
+  }
+  }
+  else{
   $title = $_POST['title'];
   $description = $_POST['description'];
   //Sql query to be executed
@@ -26,6 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
   else{
     echo "The note was not added successfully because of this error --->" . mysqli_error($conn);
   }
+}
 }
 ?>
 <!doctype html>
@@ -40,6 +55,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    <!-- Edit modal -->
+<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+  Edit Modal
+</button> -->
+
+<!-- Edit Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editModalLabel">Edit this note</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action ="/crud/index.php" method="post">
+          <input type="hidden" name="snoEdit" id="snoEdit">
+          <div class="mb-3">
+            <label for="title" class="form-label">Note Title</label>
+            <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+            </div>
+          <div>
+              <label for="floatingTextarea2">Note Description</label>
+              <textarea class="form-control" placeholder="Leave a description" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+            </div>
+            <div class="mb-3"></div>
+            <button type="submit" class="btn btn-primary">Add Note</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Nexa</a>
@@ -85,7 +135,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                 <textarea class="form-control" placeholder="Leave a description" id="description" name="description" rows="3"></textarea>
               </div>
               <div class="mb-3"></div>
-              <button type="submit" class="btn btn-primary">Add Note</button>
+              <button type="submit" class="btn btn-primary">Update Note</button>
           </form>
       </div>
       <div class="container my-4">
@@ -109,15 +159,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       <th scope='row'>". $sno . "</th>
       <td>". $row['title'] . "</td>
       <td>". $row['description'] . "</td>
-      <td> Actions </td>
+      <td> <button class='btn btn-sm btn-primary edit' id=".$row['sno'].">Edit</button> <a href='/del'>Delete</a></td>
     </tr>";
-          // echo $row['sno'] . ". Title ". $row['title'] . " Desc is ". $row['description'];
-          // echo "<br>";
         }
         ?>
-
-
   </tbody>
+  
 </table>  
       </div>
       <hr>
@@ -128,5 +175,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       $('#myTable').DataTable();
     });
   </script>
+  <script>
+    // Select all elements with the class 'edit'
+    const edits = document.getElementsByClassName('edit');
+  
+    // Convert the HTMLCollection to an array and loop through each element
+    Array.from(edits).forEach((element) => {
+      // Add an event listener for the 'click' event
+      element.addEventListener("click", (e) => {
+        console.log("edit",);
+        tr =  e.target.parentNode.parentNode;
+        title = tr.getElementsByTagName('td')[0].innerText;
+        description = tr.getElementsByTagName('td')[1].innerText;
+        console.log(title, description);
+        titleEdit.value = title;
+        descriptionEdit.value = description;
+        snoEdit.value = e.target.id;
+        console.log(e.target.id)
+        $('#editModal').modal('toggle');
+      })
+    });
+  </script>
+  
   </body>
 </html>
